df914fc7ed267cd0eec7aad5d46c4d407021e615
Flora Lopez <48796428+Flora-Java@users.noreply.github.com>
2020-03-16 16:14:41 -0400

    adding ER diagram

--- src/org/perscholas/servlets/CheckingOut.java
+++ src/org/perscholas/servlets/CheckingOut.java
@@ -15,21 +15,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 /**
  * Servlet implementation class CheckingOut
  * Description: The file checkingOut.jsp send the control to this servlet. This servlet send the control to orderReady,jsp that will show the client a 
  *   message saying that the order was accepted.
+ * 
  */
+
 @WebServlet("/CheckingOut")
 public class CheckingOut extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
+/**
+ * 
+ * @see HttpServlet#HttpServlet()
+ * 
+ */
+    
     public CheckingOut() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
